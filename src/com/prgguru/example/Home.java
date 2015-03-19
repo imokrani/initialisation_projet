86fb79f977e892a04d64d9a287e2686857466a23
imokrani <idir_mokrani@hotmail.fr>
2015-03-19 15:59:44 +0100

    Premier commit du projet WS Jersy

---
+++ src/com/prgguru/example/Home.java
@@ -0,0 +1,16 @@
+package com.prgguru.example;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Home extends Activity {
+
+	
+	  @Override
+	    protected void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        //Displays Home Screen
+	        setContentView(R.layout.home);
+	    }
+}
